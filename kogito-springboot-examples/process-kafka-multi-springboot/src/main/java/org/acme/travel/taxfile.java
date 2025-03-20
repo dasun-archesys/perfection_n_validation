547fe6e09cc9ac3dbad060282f4d303cf5322d7f
Dasun <dasun.bandara@archesys.io>
2025-03-20 14:29:55 -0600

    adding taxfile.java for the 2nd time

---
+++ kogito-springboot-examples/process-kafka-multi-springboot/src/main/java/org/acme/travel/taxfile.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.acme.travel;
+
+public class taxfile {
+
+    private String name;
+    private int income;
+    private int expenses;
+    private boolean processed;
+  
+    public taxfile(String name, int income, int expenses) {
+        this.name = name;
+        this.income = income;
+        this.expenses = expenses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public void setIncome(int income) {
+        this.income = income;
+    }
+
+    public int getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(int expenses) {
+        this.expenses = expenses;
+    }
+
+    public boolean isProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
+    }
+    
+    @Override
+    public String toString() {
+        return "Taxfile [Name=" + name + ", Income=" + income + ", Expenses=" + expenses + ", processed=" + processed + "]";
+    }
+
+}
